3fe7a19857c0261947f15d388921c7625bc4c3ed
lourolindo <epifaniovaqueiro@gmail.com>
2019-03-05 10:50:58 -0300

    Adicionando telas principais com ligações e função de logar pronta

---
+++ src/restaurante_sql/Restaurante_SQL.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurante_sql;
+
+import Classes.Usuario;
+import restaurante_sql.Conta.Abrir_Conta;
+
+
+
+/**
+ *
+ * @author Hoope
+ */
+public class Restaurante_SQL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       // Usuario u = new Usuario();
+        new Login().setVisible(true);
+    }
+    
+}
